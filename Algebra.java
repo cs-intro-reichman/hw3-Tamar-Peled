d2c9ee00f98fe965a9fda9d8d5d56d251632595f
תמר פלד <Tamar-Peled>
2024-11-26 00:52:48 +0200

    Algebra1

--- Algebra.java
+++ Algebra.java
@@ -5,6 +5,9 @@
 
 public class Algebra {
 	public static void main(String args[]) {
+
+		int x1 = Integer.parseInt(args [0]);
+		int x2 = Integer.parseInt(args[1]);
 	    // Tests some of the operations
 	    System.out.println(plus(2,3));   // 2 + 3
 	    System.out.println(minus(7,2));  // 7 - 2
@@ -24,44 +27,139 @@ public class Algebra {
 	}  
 
 	// Returns x1 + x2
+	
 	public static int plus(int x1, int x2) {
-		// Replace the following statement with your code
-		return 0;
+		while (x2 < 0) {
+			
+		
+			x1++;
+			x2--;
+			
+		}
+
+		return x1;
 	}
 
+
 	// Returns x1 - x2
-	public static int minus(int x1, int x2) {
-		// Replace the following statement with your code
-		return 0;
+	public static int minus (int x1, int x2) {
+		while (x2 != 0){
+			if (x2 > 0){
+			x1--;
+			x2--;
+	}else{ 
+			x1++;
+			x2++;
 	}
+	}
+	return x1;
+}
 
 	// Returns x1 * x2
 	public static int times(int x1, int x2) {
-		// Replace the following statement with your code
-		return 0;
+		int resultOfTimes = 0;
+
+			if (x1 == 0 || x2 == 0){
+			return 0;
+			}
+
+			if (x2 < 0){
+			x1 = -x1;
+			x2 = -x2;
+			}
+
+
+			while (x2 > 0) {
+			resultOfTimes =  plus(resultOfTimes,x1);
+			x2--;
+
+			}
+
+		
+			
+	
+		return resultOfTimes;
 	}
 
 	// Returns x^n (for n >= 0)
+	
+
 	public static int pow(int x, int n) {
-		// Replace the following statement with your code
-		return 0;
+		int resultOfPow = 1;
+
+		if (n == 0){
+			return 1;
+		}
+
+		while (n > 0) {
+		resultOfPow = times(resultOfPow , x);
+			n--;
+			
+		}
+		
+
+		return resultOfPow;
 	}
 
 	// Returns the integer part of x1 / x2 
 	public static int div(int x1, int x2) {
-		// Replace the following statement with your code
-		return 0;
+		int resultOfDiv = 0;
+		boolean isNegative = false;
+
+		if (x1 < 0 && x2 < 0 ){
+			x1 = -x1;
+			x2 = -x2;
+		}else if  ( x1 < 0 && x2 >0) {
+			isNegative = true;
+			x1 = -x1;
+		}else{
+			isNegative = true;
+			x2 = -x2;	
+		}
+		while (x1 >= x2) {
+			x1 = minus(x1, x2);
+			resultOfDiv = plus(resultOfDiv, 1);			
+		}
+
+		if (isNegative){
+			resultOfDiv = -resultOfDiv;
+		}
+		
+		
+		return resultOfDiv;
 	}
 
 	// Returns x1 % x2
 	public static int mod(int x1, int x2) {
-		// Replace the following statement with your code
-		return 0;
+		int resultOfMod = 0;
+
+		if (x1 < x2){
+		resultOfMod = x1;
+		}else if (x1 < 0 && x2 > 0){
+			resultOfMod = plus(x1, x2);
+
+		}else if (x1 == x2){
+			resultOfMod = 0;
+
+		}else{
+		int tempDiv = div(x1, x2);
+		resultOfMod = minus(x1,times(tempDiv, x2));		
+	
+		}
+			
+			
+		return resultOfMod;
 	}	
 
 	// Returns the integer part of sqrt(x) 
 	public static int sqrt(int x) {
-		// Replace the following statement with your code
+		int resultOfSqrt = 0;
+		if (x > 0) {
+		resultOfSqrt =  plus(resultOfSqrt,pow(x, div(2, 4)) );
+		}
+
+
+
 		return 0;
 	}	  	  
 }
